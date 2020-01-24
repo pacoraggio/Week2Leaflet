beee29ff1ccb5a4e71e5c243e576f60b6c21b503
pacoraggio <pacoraggio@gmail.com>
2020-01-24 16:33:30 +0000

    minor changes

--- mainScript.R
+++ mainScript.R
@@ -4,7 +4,7 @@ rm(list = ls())
 library(dplyr)
 library(ggplot2)
 library(lubridate)
-
+library(leaflet)
 # loading tided raw data
 
 load('rawdata.RData')
@@ -63,17 +63,75 @@ labels <- sprintf(
     df.plotdata$year.max
 ) %>% lapply(htmltools::HTML)
 
-head(df.plotdata)
+
+median(df.plotdata$mean.fatalities)
+
+icon.fa <- makeAwesomeIcon(icon = 'flag', 
+                           markerColor = ifelse(df.plotdata$mean.fatalities < 4.6, 'green','red'), 
+                           library='fa',
+                           iconColor = 'white')
+
+# icon.pop <- awesomeIcons(icon = icon.fa,
+#                          markerColor = ifelse(df.plotdata$mean.fatalities < 4.6,
+#                                               'green','red'),
+#                          library = 'fa',
+#                          iconColor = 'black')
+
+icon.pop <- awesomeIcons(icon = icon.fa,
+                         library = 'fa',
+                         iconColor = 'black')
+
+# head(df.plotdata)
+# df.plotdata %>%
+#     leaflet() %>%
+#     addTiles() %>%
+#     addCircles(weight = 1.7, 
+#                radius = df.plotdata$sum.fatalities *200,
+#                fillColor = "red",
+#                color = "black",
+#                fillOpacity = df.plotdata$mean.fatalities*0.04) %>%
+#     # addMarkers(icon = fatalitiesIcon, 
+#     #            label = paste("mean fatalities: ", 
+#     #                          round(df.plotdata$mean.fatalities, 2)))
+#     addAwesomeMarkers(icon = icon.fa, 
+#                label = labels) %>%
+#     addLegend(labels = c("mean fatalities < 4.6 per year",
+#                          "mean fatalities > 4.6 per year"),
+#               colors = c("green", "red"))
+# 
+
+lat1 <- min(df.capitals$Latitude) 
+lat2 <- max(df.capitals$Latitude)
+lon1 <- min(df.capitals$Longitude)
+lon2 <- max(df.capitals$Longitude)
+
+
+library(maps)
+mapStates = map("state", fill = TRUE,plot = FALSE)
+
 df.plotdata %>%
-    leaflet() %>%
+    leaflet(options = leafletOptions(minZoom = 2.8)) %>%
     addTiles() %>%
     addCircles(weight = 1.7, 
                radius = df.plotdata$sum.fatalities *200,
                fillColor = "red",
                color = "black",
                fillOpacity = df.plotdata$mean.fatalities*0.04) %>%
-    # addMarkers(icon = fatalitiesIcon, 
-    #            label = paste("mean fatalities: ", 
-    #                          round(df.plotdata$mean.fatalities, 2)))
-    addMarkers(icon = fatalitiesIcon, 
-               label = labels)
+    addAwesomeMarkers(icon = icon.fa, 
+                      label = labels) %>%
+    addLegend(labels = c("mean fatalities < 4.6 per year",
+                         "mean fatalities > 4.6 per year"),
+              colors = c("green", "red")) %>%
+    setView(lng = lon1 -20,
+            lat = lat1 -10,
+            zoom = 2.8) %>%
+    setMaxBounds( lng1 = lon1 - 10
+                  , lat1 = lat1 - 5
+                  , lng2 = lon2 + 10
+                  , lat2 = lat2 + 10)
+
+?setMaxBounds
+library(maps)
+mapStates = map("state", plot = FALSE)
+leaflet(data = mapStates) %>% addTiles() %>%
+    addPolygons(stroke = FALSE)
